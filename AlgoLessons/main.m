f307b3041730df506b30c237d61f97f2b4ad2522
Natalia Estrella <natalia.c.estrella@gmail.com>
2015-06-25 12:42:21 -0400

    Made the largest number print!

--- AlgoLessons/main.m
+++ AlgoLessons/main.m
@@ -18,8 +18,12 @@
 
 @implementation Exercise1
 
-- (int) maxIntA:(int)a intB:(int)b
-{
+- (int) maxIntA:(int)a intB:(int)b {
+    if ((int)a > (int)b) {
+        printf("%d", a);
+    }else{
+        printf("%d",b);
+    }
     // Implement the max function here
     return 0;
 }
@@ -30,8 +34,10 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"Exercise 1");
         Exercise1 *exercise1 = [[Exercise1 alloc] init];
-        NSLog(@"%@ is the larger number of either 2 or 6", @([exercise1 maxIntA:2 intB:6]));
-        NSLog(@"%@ is the larger number of either 9 or 2", @([exercise1 maxIntA:9 intB:2]));
+//        NSLog(@"%@ is the larger number of either 2 or 6", @([exercise1 maxIntA:2 intB:6]));
+//        NSLog(@"%@ is the larger number of either 9 or 2", @([exercise1 maxIntA:9 intB:2]));
+        [exercise1 maxIntA:24 intB:13];
+        
     }
     return 0;
 }
