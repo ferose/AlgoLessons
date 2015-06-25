ce4e4dd05d80aa102faa98073d8ae2f61be43492
Ferose Babu <ferose2@gmail.com>
2015-06-25 11:07:52 -0400

    Initial Commit

---
+++ AlgoLessons/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AlgoLessons
+//
+//  Created by Ferose Babu on 6/25/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
